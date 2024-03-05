9a9b31780a4dc08b5217d68814b940fe9ca72757
Joshua Cho <jcho129@ucsc.edu>
2024-03-05 15:59:07 -0800

    first commit till 2 hour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accordian from "./components/Accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import LoadMoreData from "./components/load-more-data";
+
+
+import TreeView from "./components/tree-view";
+
+import menus from "./components/tree-view/data";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Accordian /> */}
+      {/* <RandomColor /> */}
+      {/* <StarRating noOfStars={10}/> */}
+      {/* <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> */}
+      {/* <LoadMoreData /> */}
+      <TreeView menus={menus} /> 
+
+
+
+      
     </div>
   );
 }
